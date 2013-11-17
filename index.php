2f71609da7c87c8a2c49c4346fc11333f39dfc19
Christian Landin <christian.landin@me.com>
2013-11-17 22:16:47 +0100

    Base template

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=utf-8">
+        <title>Bootstrap 3</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="_/css/bootstrap.css">
+        <link rel="stylesheet" href="_/css/mystyles.css">
+        <script src="_/js/bootstrap.js"></script>
+        <script src="_/js/myscript.js"></script>
+        
+    </head>
+    <body>
+        <h1>Test Site</h1>
+    </body>
+</html>
